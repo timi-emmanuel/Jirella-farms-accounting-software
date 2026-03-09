2eeb43769ac700567b0c0191d32dd0d1e19069c7
timi-emmanuel <121458382+timi-emmanuel@users.noreply.github.com>
2026-03-09 23:46:31 +0100

    fix for deployment issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthLogger } from "@/components/AuthLogger";
 import { Toaster } from "@/components/ui/toaster";
 import { AlertToastBridge } from "@/components/AlertToastBridge";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Jirella Farms Software",
@@ -27,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden`}
-      >
+      <body className="antialiased overflow-hidden">
         <AuthLogger />
         <AlertToastBridge />
         <Toaster />
